88a0c1f4a58681601629b2ec50369cefbe2cc219
Parker Lougheed <parlough@gmail.com>
2024-02-07 16:31:55 -0600

    Remove no longer supported workshops (#2167)
    Removes the workshops previously supported by DartPad. I updated the
    [README](https://github.com/flutter/codelabs/blob/8ee91e1ecf503eeb2cc0aba4bd0d9b32c7424a36/dartpad_codelabs/README.md)
    with more context about their removal in case anyone stumbles upon the
    page or there's a link remaining somewhere. This can probably be removed
    in a few months.
    
    For some extra context, I removed related mentions/links from the
    website in https://github.com/flutter/website/pull/9908.
    
    Thanks everyone who worked on these great workshops! I hope we can
    revive some of their contents in a different form in the future :)
    
    Resolves https://github.com/dart-lang/dart-pad/issues/2749

--- firebase-auth-flutterfire-ui/start/lib/auth_gate.dart
+++ firebase-auth-flutterfire-ui/start/lib/auth_gate.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'home.dart';
@@ -7,6 +9,17 @@ class AuthGate extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const HomeScreen();
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return SignInScreen(
+            providers: [EmailAuthProvider()],
+          );
+        }
+
+        return const HomeScreen();
+      },
+    );
   }
 }
--- firebase-auth-flutterfire-ui/start/lib/home.dart
+++ firebase-auth-flutterfire-ui/start/lib/home.dart
@@ -12,7 +12,7 @@ class HomeScreen extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
-            Image.asset('dash.png'),
+            Image.asset('assets/images/dash.png'),
             Text(
               'Welcome!',
               style: Theme.of(context).textTheme.displaySmall,
